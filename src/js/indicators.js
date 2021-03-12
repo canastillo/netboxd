0f450a97eae293f25656231875c7b0887946d61d
Ana Castillo <canastillo_@outlook.com>
2021-03-11 18:44:59 -0600

    BEM y code split
    Terminada la corrección de las clases con BEM. División del código de css y reordenamiento de carpetas

--- src/js/indicators.js
+++ src/js/indicators.js
@@ -9,27 +9,27 @@ function makeIndicators(lists){
         let numPages = Math.ceil(movies.length/mpp);                            // cant de páginas
         
         for(let i = 0; i < numPages; i++){
-            var page = document.createElement('button');// creamos un indicador por página
-            if(i === 0) page.classList.add('activo');   // marcamos siempre el primer indicador
-            contPages.appendChild(page);                // agregamos el indicador
+            var page = document.createElement('button');           // creamos un indicador por página
+            page.classList.add('indicator')                        // añadimos el estilo
+            if(i === 0) page.classList.add('indicator--active');   // marcamos siempre el primer indicador
+            contPages.appendChild(page);                           // agregamos el indicador
             
             page.addEventListener('click', (e) => {                 // cada que hagan click en un indicador
                 var pages = [].slice.call(contPages.children);      // traemos los indicadores en su estado actual
             
                 pages.forEach( p => {                               // removemos la clase 'activo' a cada indicador
-                    if (p.classList.contains('activo')) p.classList.remove('activo')
+                    if (p.classList.contains('indicator--active')) 
+                        p.classList.remove('indicator--active')
                 })
                 
-                e.target.classList.add('activo');                   // activamos el indicador cliqueado
+                e.target.classList.add('indicator--active');                   // activamos el indicador cliqueado
                 row.scrollLeft = i * row.offsetWidth;               // movemos el scroll
             });
         }
     
         row.addEventListener('mouseleave', () => {
-            movies.forEach(movie => movie.classList.remove('hover'));
+            movies.forEach(movie => movie.classList.remove('movie--hover'));
         })
-        
-
     })
 }
 
