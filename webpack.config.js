3a92d6818aac8cac811f6f4587042de5f92e9ace
Ana Castillo <canastillo_@outlook.com>
2021-04-19 11:42:50 -0500

    html-loader uninstall

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.html$/i,
+                test: /\.(html)$/,
                 use: [
                     {
                         loader: 'html-loader',
